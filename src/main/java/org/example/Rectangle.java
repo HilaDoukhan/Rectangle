b572f522e317a6d475d8c61566b91976af1f5659
Hila <hila@gmail.com>
2023-07-12 23:58:26 +0300

    Initial commit

---
+++ src/main/java/org/example/Rectangle.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Rectangle extends JFrame {
+
+//    public Rectangle(){
+//        this.setSize(500, 500);
+//        this.setResizable(false);
+//        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        this.setLayout(null);
+//        this.setLocationRelativeTo(null);
+//        this.setTitle("title");
+//        Panel Panel = new Panel(0,0, 500, 500);
+//        this.add(Panel);
+//        this.setVisible(true);
+
+
+    }
+
+
